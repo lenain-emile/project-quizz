f67ccb52dc66d12b056bb164b3148aaa6e1c5cf1
AlexisNguemby <alexis.nguemby-mbina@laplateforme.io>
2025-02-10 16:08:57 +0100

    Structure of the homePage and the css

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+   
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Quizz</title>
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/flex.css">
+</head>
+<body>
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="about.php">A propos</a></li>
+            <li><a href="contact.php">Compte</a></li>
+        </ul>
+    </nav>
+</header>
+</header>
+<div class="container">
+    <div class="item">
+        <h2>Titre 1</h2>
+        <a href="http://example.com"><img src="image/quizz.png" alt="Description of image"></a>
+       
+    </div>
+    <div class="item">
+        <h2>Titre 2</h2>
+        <a href="http://example.com"><img src="image/quizz.png" alt="Description of image"></a>
+        
+    </div>
+    <div class="item">
+        <h2>Titre 3</h2>
+        <a href="http://example.com"><img src="image/quizz.png" alt="Description of image"></a>
+        
+    </div>
+</div>
+
+<div class="container">
+    <div class="item">
+        <h2>Titre 4</h2>
+        <a href="http://example.com"><img src="image/quizz.png" alt="Description of image"></a>
+        
+    </div>
+    <div class="item">
+        <h2>Titre 5</h2>
+        <a href="http://example.com"><img src="image/quizz.png" alt="Description of image"></a>
+        
+    </div>
+    <div class="item">
+        <h2>Titre 6</h2>
+        <a href="http://example.com"><img src="image/quizz.png" alt="Description of image"></a>
+        
+    </div>
+</div>
+
+</body>
+</html>

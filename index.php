f5df036b2bbae8b6fb32b41e1202508942a982b1
AlexisNguemby <alexis.nguemby-mbina@laplateforme.io>
2025-02-11 17:53:27 +0100

    changes

--- index.php
+++ index.php
@@ -13,53 +13,64 @@
     <title>Project Quizz</title>
     <link rel="stylesheet" href="style/style.css">
     <link rel="stylesheet" href="style/flex.css">
+<link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap" rel="stylesheet">
+    
 </head>
 <body>
-<header>
-    <nav>
-        <ul>
-            <li><a href="index.php">Accueil</a></li>
-            <li><a href="about.php">A propos</a></li>
-            <li><a href="contact.php">Compte</a></li>
-        </ul>
-    </nav>
-</header>
-</header>
+<nav class="navbar"></nav>
+    <div class="navbar-container">
+        <input type="checkbox" id="navbar-toggle">
+        <label for="navbar-toggle" class="navbar-icon">&#9776;</label>
+        <div class="navbar-menu">
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="user.php">Profil</a>
+        </div>
+    </div>
+</nav>
+<main>
+<h1>Bienvenue sur Quizz !</h1>
+
+<p>Êtes-vous prêt à tester vos connaissances et à relever des défis passionnants ? 
+À vous de jouer !</p>
 <div class="container">
     <div class="item">
-        <h2>Titre 1</h2>
+        
         <a href="http://example.com"><img src="image/quizz.png" alt="Description of image"></a>
-       
+        <h2>Manga</h2>
     </div>
     <div class="item">
-        <h2>Titre 2</h2>
-        <a href="http://example.com"><img src="image/quizz.png" alt="Description of image"></a>
         
+        <a href="http://example.com"><img src="image/quizz.png" alt="Description of image"></a>
+        <h2>Football</h2>
     </div>
     <div class="item">
-        <h2>Titre 3</h2>
-        <a href="http://example.com"><img src="image/quizz.png" alt="Description of image"></a>
         
+        <a href="http://example.com"><img src="image/quizz.png" alt="Description of image"></a>
+        <h2>Serie TV</h2>
     </div>
 </div>
 
 <div class="container">
     <div class="item">
+        
+        <a href="http://example.com"><img src="image/quizz.png" alt="Description of image"></a>
         <h2>Titre 4</h2>
-        <a href="http://example.com"><img src="image/quizz.png" alt="Description of image"></a>
-        
     </div>
     <div class="item">
+        
+        <a href="http://example.com"><img src="image/quizz.png" alt="Description of image"></a>
         <h2>Titre 5</h2>
-        <a href="http://example.com"><img src="image/quizz.png" alt="Description of image"></a>
-        
     </div>
     <div class="item">
-        <h2>Titre 6</h2>
-        <a href="http://example.com"><img src="image/quizz.png" alt="Description of image"></a>
         
+        <a href="http://example.com"><img src="image/quizz.png" alt="Description of image"></a>
+        <h2>Titre 6</h2>
     </div>
 </div>
+</main>
+
+
 
 </body>
 </html>
